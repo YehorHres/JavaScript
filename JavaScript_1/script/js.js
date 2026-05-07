2c4a71e590ade067bf0a7c97a86ca799d4956e1e
Yehor Hres <yehorhres07@gmail.com>
2026-05-07 10:17:05 +0300

    Add files via upload

---
+++ JavaScript_1/script/js.js
@@ -0,0 +1,74 @@
+// console.log("Hello JS!");
+// console.info("Hello JS#2");
+// console.error("Hello JS#3");
+// console.warn("Hello JS#4");
+
+
+// var MyFirstVar;
+// MyFirstVar = 10;
+// console.log(MyFirstVar);
+//
+// var MySecondVar = '2';
+// console.log("Змінна: " + MySecondVar);
+// console.log(MySecondVar + MyFirstVar);
+// console.log(MyFirstVar - MySecondVar);
+
+// var first, second = 2, '4';
+
+// var int = 10;
+// var float = 10.2;
+// var BOOL = true; //false
+// var str = 'str';
+// var nothing;
+// nothing = null;
+
+
+// num = 2
+// var num;
+// console.log(num)
+
+
+// let num = 13;
+// num = '14'
+// console.log(num);
+//
+// const Pi = 3.14;
+
+
+// МАТЕМАТИЧНІ ОПЕРАТОРИ
+
+
+// let a = 21,b = 4;
+// console.log(a+b);
+// console.log(a-b);
+// console.log(a * b);
+// console.log(a / b);
+// console.log(a % b);
+// console.log(a ** b);
+// let a, b;
+// // a = 2 + 4;
+// // b = 2 * (a = 10 + 3);
+// a = a + 1;
+// a += 1;
+//
+// a++; //інкремент
+// a--; //декремент
+//
+// ++a;
+// --a;
+
+// let a = 2, b;
+// b = ++a;
+// console.log(b)
+
+// let a = '2', b = '3';
+// console.log((a - 0)+(b - 0));
+// console.log(Number(a) + Number(b));
+
+// Щеплення(конкатенація), інтерполяція
+
+let name = 'Ivan';
+console.log("Привіт " + name +'!');
+let skill = "WEB";
+
+console.log(`Привіт ${name}! Твій найкращий скіл - це ${skill} ${2 * 4}`);
